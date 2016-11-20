5a8a7b0620877660a97be1321b9a59163fb5fec0
William Sanders <william.sanders.dev@gmail.com>
2016-11-20 17:56:11 -0500

    login view

--- src/GolfBag/Controllers/AccountController.cs
+++ src/GolfBag/Controllers/AccountController.cs
@@ -1,4 +1,6 @@
-﻿using GolfBag.ViewModels;
+﻿using GolfBag.Entities;
+using GolfBag.ViewModels;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -9,6 +11,15 @@ namespace GolfBag.Controllers
 {
     public class AccountController : Controller
     {
+        private SignInManager<User> _signInManager;
+        private UserManager<User> _userManager;
+
+        public AccountController(UserManager<User> userManager,
+            SignInManager<User> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
         [HttpGet]
         public IActionResult Register()
         {
@@ -16,9 +27,67 @@ namespace GolfBag.Controllers
         }
 
         [HttpPost, AutoValidateAntiforgeryToken]
-        public IActionResult Register(RegisterUserViewModel model)
+        public async Task<IActionResult> Register(RegisterUserViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new User { UserName = model.Username };
+
+                var createResult = await _userManager.CreateAsync(user, model.Password);
+                if (createResult.Succeeded)
+                {
+                    await _signInManager.SignInAsync(user, false);
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    foreach (var error in createResult.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);  //these errors will appear in validation summary because they're not associated with a specific model
+                    }
+                }
+            }
+            return View();
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<IActionResult> Logout ()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+        
+        [HttpGet]
+        public IActionResult Login()
         {
             return View();
         }
+        
+        [HttpPost]
+        public async Task<IActionResult> Login (LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var loginResult = await _signInManager.PasswordSignInAsync(
+                    model.Username,
+                    model.Password,
+                    model.RememberMe,
+                    false);
+
+                if (loginResult.Succeeded)
+                {
+                    if (Url.IsLocalUrl(model.ReturnUrl))
+                    {
+                        return Redirect(model.ReturnUrl);
+                    }
+                    else
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+            }
+            ModelState.AddModelError("", "Could not log in");
+            return View(model);
+        } 
     }
 }
