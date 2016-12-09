dfed0bb4104ef6d28ac91efb6c6c09dadfa1cf52
William Sanders <william.sanders.dev@gmail.com>
2016-12-08 21:26:10 -0500

    improved striped table to change stripes when teeboxes are toggled

---
+++ src/GolfBag/wwwroot/js/_DisplayRound.js
@@ -0,0 +1,24 @@
+﻿// This stripes the scorecard and restripes it when toggling teeboxes
+$(function () {
+    "use strict";
+    
+    var stripeTables = function () {
+        $(".table-striped-custom").each(function (i) {
+            var x = 2;
+            $(this).find("tr").each(function (i) {
+                $(this).removeClass("table-row-striped");
+                if ((!($(this).hasClass("collapse")) && (x % 2 === 0))
+                        || ($(this).hasClass("in")) && (x % 2 === 0)) {
+                    $(this).addClass("table-row-striped");
+                    x++;
+                } else if ((!($(this).hasClass("collapse")))
+                        || ($(this).hasClass("in"))) {
+                    x++;
+                }               
+            });
+        });
+    };
+
+    $(document).ready(stripeTables);
+    $(document).on("shown.bs.collapse hidden.bs.collapse", ".collapse", stripeTables);
+});
