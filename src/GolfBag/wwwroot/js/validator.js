c1f688d1af6ab4857df74d1d696acd05c8bbae85
William Sanders <william.sanders.dev@gmail.com>
2017-01-11 21:37:04 -0500

    improved validation scorecard

--- src/GolfBag/wwwroot/js/validator.js
+++ src/GolfBag/wwwroot/js/validator.js
@@ -5,19 +5,38 @@
 var validateForm = function ($form) {
     "use strict";
 
+    var validateClass = function ($element) {
+        if (!($element.valid())) {
+            $element.addClass("invalid-score");
+        } else {
+            $element.removeClass("invalid-score");
+        }
+    };
+
     $form.validate();
+
     $(".error-container").empty();
 
     $(".score").each(function () {
-        var $holeNumber = $(this).attr("data-hole-number");
-        $(this).rules("add", {
+        var $holeNumber = $(this).attr("data-hole-number"),
+            $element = $(this);
+        $element.rules("add", {
             required: true,
             range: [1, 99],
             messages: {
                 required: "Hole " + $holeNumber + ": You must enter a score",
                 range: "Hole " + $holeNumber + ": Score must be between 1 and 99"
             }
-        })
+        });
+        $element.on("focusout focusin keyup", function () {     //RETURN FALSE IF TAB KEY INSTEAD OF FOCUSIN
+            validateClass($element);
+        });
+    });
+
+    $form.on("invalid-form.validate", function () {
+        $(".score").each(function () {
+            validateClass($(this));
+        });
     });
 
     $form.data("validator").settings.onfocusout = function (element) { $(element).valid(); };
