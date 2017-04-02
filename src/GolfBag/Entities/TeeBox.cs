9e1f88621316df5da0183b12b8bb83870abeacc5
William Sanders <william.sanders076@gmail.com>
2017-04-01 22:31:03 -0400

    Added 18-hole total for yardages.

--- src/GolfBag/Entities/TeeBox.cs
+++ src/GolfBag/Entities/TeeBox.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -12,5 +13,31 @@ namespace GolfBag.Entities
         public string Name { get; set; }
 
         public List<Tee> Tees { get; set; }
+
+        //[NotMapped]
+        //public int SumFrontYardage
+        //{
+        //    get
+        //    {
+        //        if (Tees.Count >= 9)
+        //        {
+        //            return Tees.Take(9).Sum(x => x.Yardage);
+        //        }
+        //        return 0;
+        //    }
+        //}
+
+        [NotMapped]
+        public int TotalYardage
+        {
+            get
+            {
+                if (Tees.Count == 18)
+                {
+                    return Tees.Sum(x => x.Yardage);
+                }
+                return 0;
+            }
+        }
     }
 }
--- src/GolfBag/ViewModels/RoundOfGolfViewModel.cs
+++ src/GolfBag/ViewModels/RoundOfGolfViewModel.cs
@@ -91,7 +91,6 @@ namespace GolfBag.ViewModels
             }
         }
 
-
         public RoundOfGolfViewModel()
         {
             CurrentDate = DateTime.Today.Date.ToString("D");
